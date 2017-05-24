ac8dcb9f2573c653a4ac5d1dc5fd960b99fa193f
Andrew Bodey <a.s.bodey@gmail.com>
2017-05-23 22:29:23 -0700

    COMPLETED TODOS with kens help

--- public/scripts/controllers/aboutController.js
+++ public/scripts/controllers/aboutController.js
@@ -1,14 +1,13 @@
 'use strict';
 
-(function(module) {
+(function (module) {
   const aboutController = {};
 
   aboutController.index = () => {
     $('#about').show().siblings().hide(); // REVIEW: We have a slight refactor in selectors here, which has reduced the amount of code from the last lab.
-
-    // TODO: Call a function to load all the data.
+    // DONE: Call a function to load all the data.
+    repos.requestRepos(repoView.index);
     // Pass a view function as a callback, so the view will render after the data is loaded.
-    
   };
 
   module.aboutController = aboutController;
--- public/scripts/views/repoView.js
+++ public/scripts/views/repoView.js
@@ -1,26 +1,29 @@
 'use strict';
 
-(function(module) {
+(function (module) {
   const repoView = {};
 
   // REVIEW: Private methods declared here live only within the scope of the wrapping IIFE.
-  const ui = function() {
+  const ui = function () {
     let $about = $('#about'); // Best practice: Cache the DOM query if it's used more than once.
 
     $about.find('ul').empty();
     $about.show().siblings().hide();
   };
 
-  // TODO: Remember that new Handlebars template? Let's compile it!
+  // DONE: Remember that new Handlebars template? Let's compile it!
+  var render = Handlebars.compile($('#repo-template').text());
   // Save the result in this `render` variable.
 
-  repoView.index = function() {
+  repoView.index = function () {
+
     ui();
 
     // The jQuery `append` method lets us append an entire array of HTML elements at once:
     $('#about ul').append(
       repos.with('name').map(render) // Want to filter by a different property other than name?
     );
+
   };
 
   module.repoView = repoView;
